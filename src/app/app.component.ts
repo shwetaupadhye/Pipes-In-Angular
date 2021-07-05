eb9830c5b418e04ec17e4383ee22e32ef8329f12
Shweta Upadhye <shwetauoadhye777@gmail.com>
2021-07-05 10:59:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
+
 })
+
 export class AppComponent {
   title = 'pipesInAngular';
+
+  todaydate = new Date();
+  info = { name: 'Jun', age: '25', address: { a1: 'Paris' } };
+  months = ['Jan', 'Feb', 'Mar', 'April', 'May', 'Jun',
+    'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
 }
